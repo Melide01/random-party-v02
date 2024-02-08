1365cb89adae2094f15dd008eddcd71e97d2040c
Melide01 <56476369+Melide01@users.noreply.github.com>
2024-02-07 14:54:43 -1000

    Update random-items.js

--- random-items.js
+++ random-items.js
@@ -3,8 +3,6 @@ var randomItems = [
   '<input type="range" id="items" data-type="slider" data-event="hide" min="0" max="100" value="50" step="1">',
   '<button id="items" class="btn" data-event="none">Appuie sur le bouton</button>',
   '<img id="items" src="assets/silly-cat-holder.png" data-event="none">',
-  'img_artwork1.png',
-  'vid_blue-screen-subasacaraib.MP4',
   'img_aesthetic.jpg',
   'img_bg.jpg',
   'img_cafe.PNG',
