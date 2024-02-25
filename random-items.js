2e718c579c97493eb885f95f6b2ee47cfd017253
Melide01 <56476369+Melide01@users.noreply.github.com>
2024-02-24 17:04:05 -1000

    Update random-items.js

--- random-items.js
+++ random-items.js
@@ -11,13 +11,13 @@ var randomItems = [
   '<p id="items">met des confetis sur l amour toujours</p>',
   '<p id="items">regarde jusqu a la fin</p>',
   '<p id="items">pour recevoir un bon cadeau</p>',
+  'img_confet.png',
 
   '<div id="items"><a href="#" id="imageSelector">Inserer une image ici<input type="file" style="display: none;" id="imageInput" accept="image/*"></a></div>',
-
-  '<p id="items"></p>',
-  '<p id="items"></p>',
   '<a id="items" href="RandomFolder/specials/others/NougatinePackfromMelide-curated.zip" download="NougatinePackfromMelide-curated.zip">telecharger le sample pack Nougatine!</a>',
-  '',
+  
+  'img_jdst.png',
+  'img_pouet.png',
   'vid_loop-conffetis.mp4',
 
   '<p id="items" data-event="none">keep going down</p>',
